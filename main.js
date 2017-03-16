950a2031d8fe3fafeef535d6ed05c7c27894163a
Devin Gray <devin.gray@colorado.edu>
2017-03-16 14:58:44 -0600

    tis working

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
+var climate,terrain=""
+var returnedPlanets
+function displayPlanet(){
+  if(climate && terrain){
+    var url="http://swapi.co/api/planets"
+    $.get(url)
+
+    .then(function(data){
+      console.log(data)
+      for (var i = 0; i < data.results.length; i++) {
+        // console.log(data.results[i].name)
+        if(climate===data.results[i].climate && terrain===data.results[i].terrain){
+          console.log(data.results[i].name)
+
+          returnedPlanets = data.results[i].name;
+          var planet = returnedPlanets
+          document.querySelector('#planetHere').textContent = "Vacation on: " + planet
+          return data.results[i].name;
+        }
+      }
+      return "Nothing matched that search, try again!"
+    })
+  }
+
+}
 
 $(document).ready(function() {
+    Materialize.updateTextFields();
+
   $('#dropdown1').click(function(event){
     console.log(event.target.innerHTML);
     document.querySelector('#textFieldId').textContent = "selected climate: "
     document.querySelector('#textFieldId').append(event.target.innerHTML);
+    climate=event.target.innerHTML
     searchClimate()
   })
 
@@ -11,12 +39,26 @@ $(document).ready(function() {
     console.log(event.target.innerHTML);
     document.querySelector('#secondText').textContent = "selected terrain: "
     document.querySelector('#secondText').append(event.target.innerHTML);
+    terrain=event.target.innerHTML
     searchTerrain()
   })
 
+  $('#findPlanet').click(function(event){
+    displayPlanet()
+    console.log(returnedPlanets);
+    var planet = returnedPlanets
+    document.querySelector('#planetHere').textContent = "Vacation on: " + planet
 
 
- });
+
+  })
+})
+
+//get planet function
+
+
+
+
 
 // get climate function
 function searchClimate() {
@@ -54,9 +96,7 @@ function searchTerrain() {
 
 
 // For customer input
- $(document).ready(function() {
-    Materialize.updateTextFields();
-  });
+
 
   $('#textarea1').val('');
 $('#textarea1').trigger('autoresize');

e0ef0298f869181c03c36fd7f4f43dfed19eb3b3
Devin Gray <devin.gray@colorado.edu>
2017-03-16 09:32:19 -0600

    text appends to screen

--- main.js
+++ main.js
@@ -13,11 +13,14 @@ $(document).ready(function() {
 // click climate
  $('#dropdown1').click(function(event){
    console.log(event.target.innerHTML);
-   document.querySelector('#textFieldId').textContent = "selected climate"
+   document.querySelector('#textFieldId').textContent = "selected climate: "
+   document.querySelector('#textFieldId').append(event.target.innerHTML);
  })
 // click terrain
  $('#dropdown2').click(function(event){
    console.log(event.target.innerHTML);
+   document.querySelector('#secondText').textContent = "selected climate: "
+   document.querySelector('#secondText').append(event.target.innerHTML);
  })
 
 // For customer input
@@ -27,3 +30,16 @@ $(document).ready(function() {
 
   $('#textarea1').val('');
 $('#textarea1').trigger('autoresize');
+
+
+// date picker
+$('.datepicker').pickadate({
+   selectMonths: true, // Creates a dropdown to control month
+   selectYears: 15 // Creates a dropdown of 15 years to control year
+ });
+
+// index3html clear button
+function eraseText() {
+    document.getElementById("output").value = "";
+    console.log("alert");
+}

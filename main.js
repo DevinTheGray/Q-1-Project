897dca54dfdd9a4d255fdfaac80a89817dce4c0a
Devin Gray <devin.gray@colorado.edu>
2017-03-16 16:16:37 -0600

    matches

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 var climate,terrain=""
 var returnedPlanets
+var matchClimate=[]
+var matchTerrain=[]
 function displayPlanet(){
   if(climate && terrain){
     var url="http://swapi.co/api/planets"
     $.get(url)
 
     .then(function(data){
-      console.log(data)
+      // console.log(data)
       for (var i = 0; i < data.results.length; i++) {
         // console.log(data.results[i].name)
         if(climate===data.results[i].climate && terrain===data.results[i].terrain){
-          console.log(data.results[i].name)
+          // console.log(data.results[i].name)
 
           returnedPlanets = data.results[i].name;
           var planet = returnedPlanets
           document.querySelector('#planetHere').textContent = "Vacation on: " + planet
           return data.results[i].name;
         }
+
+
       }
+
       return "Nothing matched that search, try again!"
     })
   }
@@ -28,15 +33,17 @@ $(document).ready(function() {
     Materialize.updateTextFields();
 
   $('#dropdown1').click(function(event){
-    console.log(event.target.innerHTML);
+    matchClimate=[]
+    // console.log(event.target.innerHTML);
     document.querySelector('#textFieldId').textContent = "selected climate: "
     document.querySelector('#textFieldId').append(event.target.innerHTML);
     climate=event.target.innerHTML
-    searchClimate()
+    searchClimate(climate)
   })
 
   $('#dropdown2').click(function(event){
-    console.log(event.target.innerHTML);
+    matchTerrain=[]
+    // console.log(event.target.innerHTML);
     document.querySelector('#secondText').textContent = "selected terrain: "
     document.querySelector('#secondText').append(event.target.innerHTML);
     terrain=event.target.innerHTML
@@ -45,7 +52,7 @@ $(document).ready(function() {
 
   $('#findPlanet').click(function(event){
     displayPlanet()
-    console.log(returnedPlanets);
+    // console.log(returnedPlanets);
     var planet = returnedPlanets
     document.querySelector('#planetHere').textContent = "Vacation on: " + planet
 
@@ -61,24 +68,36 @@ $(document).ready(function() {
 
 
 // get climate function
-function searchClimate() {
+function searchClimate(climate) {
   var url = "http://swapi.co/api/planets"
   $.get(url)
   .then(function(data){
+    console.log(data)
     for (var i = 0; i < data.results.length; i++) {
-      console.log(data.results[i].climate);
-
+      var climateData= data.results[i].climate.split(", ");
+      for (var j = 0; j < climateData.length; j++) {
+        if (climateData[j] === climate) {
+          matchClimate.push(data.results[i].name)
+        } else {
+          console.log("no match")
+        }
+      }
     }
-
-
-
-
-
-    var name = data.results.length
-    // console.log(name)
-    // updatePageOne(name, tagline)
+          console.log("Matching Climates: " +matchClimate)
   })
 }
+
+
+
+
+
+
+
+    // var name = data.results.length
+    // console.log(name)
+    // updatePageOne(name, tagline)
+//   })
+// }
 //get terrain function
 
 function searchTerrain() {
@@ -86,7 +105,7 @@ function searchTerrain() {
   $.get(url)
   .then(function(data){
     for (var i = 0; i < data.results.length; i++) {
-      console.log(data.results[i].terrain);
+      // console.log(data.results[i].terrain);
 
     }
     var name= data.results.length
